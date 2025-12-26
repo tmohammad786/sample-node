b46a79411050d8b0f44ee60d803e129505029e9f
Ubuntu <ubuntu@ip-172-31-11-133.ap-south-1.compute.internal>
2025-12-26 14:15:07 +0000

    Initial simple node app

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Node.js running on EC2 🚀');
+});
+
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
+
